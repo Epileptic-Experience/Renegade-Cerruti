418bd01a4361a17427eb06d06fcd905780552f59
unknown <joaquincerruti54@gmail.com>
2022-05-05 21:55:40 -0300

    desafio 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>renegade:</h1>
+          <h2>Comics and Collectables</h2>
         </p>
         <a
           className="App-link"
