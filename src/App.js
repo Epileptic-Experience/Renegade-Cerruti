9dd52b4729cd59344238388755eeb3311b75e5af
unknown <joaquincerruti54@gmail.com>
2022-05-11 16:35:31 -0300

    desafio2: creado navbar con boostrap y fonts google para el titulo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>renegade:</h1>
-          <h2>Comics and Collectables</h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <NavBar/>
     </div>
+
+
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,36 @@
+import './navbar.css';
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-xlg navbar-dark bg-dark">
+            <div className="container-fluid">
+
+                <a className="navbar-brand" href="#">
+                   
+                    <p className='marca'>renegade </p>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">comics</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">collectables</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar
