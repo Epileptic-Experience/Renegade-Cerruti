fda219d855bdda34f7aebebf218634242b10e5c1
unknown <joaquincerruti54@gmail.com>
2022-06-02 14:49:42 -0300

    primera entrega parcial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,7 +52,6 @@ const NavBar = () => {
                                     </Link>
 
                                 })}
-
                             </div>
                         </NavDropdown>
 
